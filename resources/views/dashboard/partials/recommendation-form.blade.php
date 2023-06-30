67ef5021a0cb7ffb03a44f9d135fc86003aa3ec6
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-06-30 19:07:18 +0200

    show recommendations form

--- resources/views/dashboard/partials/recommendation-form.blade.php
+++ resources/views/dashboard/partials/recommendation-form.blade.php
@@ -1,24 +1,45 @@
-<section class="space-y-6 p-5 ml-[30%]">
-    <header>
-        <img src="/storage/{{$user->avatar}}" alt="user avatar" class="rounded w-[50%]">
-
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 inline-block">
-            {{$user->name}}
-        </h2>
-
-        <p class="mt-1 ml-1 inline-block text-xs text-gray-600 dark:text-gray-400">
-            Joined {{$user->created_at->year}}
+<section class="space-y-6 p-5">
+    @if (is_null($user))
+        <p class="mt-1 mr-2 text-xs text-gray-600 dark:text-gray-400">
+            Wow, you reviewed everyone...
         </p>
+    @else
+        <header class="w-[300px]">
+            <img src="/storage/{{$user->avatar}}" alt="user avatar" class="rounded max-w-[100%]">
 
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{$user->description}}
-        </p>
-    </header>
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 inline-block">
+                {{$user->name}}
+            </h2>
 
-    <x-secondary-button
-        x-data=""
-    >{{ __('Like') }}</x-secondary-button>
-    <x-secondary-button
-        x-data=""
-    >{{ __('Dislike') }}</x-secondary-button>
+            <p class="mt-1 ml-1 inline-block text-xs text-gray-600 dark:text-gray-400">
+                Joined {{$user->created_at->year}}
+            </p>
+
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                {{$user->description}}
+            </p>
+        </header>
+
+        <form class="hidden" id="relationForm" method="post" action="{{route('createRelation')}}">
+            @csrf
+            <input name="target_id" id="target_id" type="hidden" value="{{$user->id}}">
+            <input name="likes" id="likes" type="hidden">
+        </form>
+
+        <script defer>
+            function makeGoodRelation(likes) {
+                document.getElementById('likes').value=likes;
+                document.getElementById('relationForm').submit();
+            }
+        </script>
+
+        <x-secondary-button
+            x-data=""
+            onclick="makeGoodRelation(1);"
+        >{{ __('Like') }}</x-secondary-button>
+        <x-secondary-button
+            x-data=""
+            onclick="makeGoodRelation(0);"
+        >{{ __('Dislike') }}</x-secondary-button>
+    @endif
 </section>

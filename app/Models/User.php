134fef62e589cb16b5359b12c43389b6601aba62
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-06-30 16:11:38 +0200

    user relations in model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -44,4 +45,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function created_relations(): HasMany
+    {
+        return $this->hasMany(Relation::class, "creator_id", "id");
+    }
+
+    public function linked_relations(): HasMany
+    {
+        return $this->hasMany(Relation::class, "target_id", "id");
+    }
 }

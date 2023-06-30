30349df7ca3372d6f09780dcca73fdb411ff3d71
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-06-30 13:06:42 +0200

    display and submit avatar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,6 +23,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            <img class="w-10 mr-2 rounded-full" src="/storage/{{ Auth::user()->avatar }}" alt="avatar"/>
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -13,7 +13,7 @@
         @csrf
     </form>
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6" enctype="multipart/form-data">
         @csrf
         @method('patch')
 
@@ -21,6 +21,12 @@
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
+        </div>
+
+        <div>
+            <x-input-label for="avatarfile" :value="__('Avatar')" />
+            <x-text-input id="avatarfile" name="avatarfile" type="file" class="mt-1 block w-full" autofocus />
+            <x-input-error class="mt-2" :messages="$errors->get('avatarfile')" />
         </div>
 
         <div>

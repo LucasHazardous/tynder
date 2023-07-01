a800973dbbd68feb001165c40924f5667ebb6396
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-07-01 12:56:23 +0200

    send message form

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg h-[500px]">
             @if(empty($messages))
                 @foreach ($users as $user)
                 <a href="{{route('chat')}}/{{$user->id}}">
@@ -19,10 +19,18 @@
                 @endforeach
             @else
                 @foreach ($messages as $message)
-                <div class="p-3 border-solid border-2 border-indigo-800 rounded-md mb-3">
+                <div class="p-3 border-solid border-2 {{ $message->sender === auth()->user()->id ? 'border-indigo-800' : 'border-indigo-500' }} rounded-md mb-3">
                     <p class="inline-block text-lg font-medium text-gray-900 dark:text-gray-100">{{$message->content}}</p>
                 </div>
                 @endforeach
+                <div class="p-3 border-solid border-2 border-indigo-800 rounded-md mb-3">
+                    <form action="{{route('chat.message.send')}}" method="post">
+                        @csrf
+                        <input type="hidden" name="receiver" id="receiver" value="{{$receiver}}">
+                        <x-text-input class="min-w-[250px]" type="text" name="content" id="content" maxlength="255" minlength="2" required autofocus />
+                        <x-primary-button class="float-right">{{ __('Save') }}</x-primary-button>
+                    </form>
+                </div>
             @endif
             </div>
         </div>

034be14e9e3337febe3f36d8f8935832dd91ff85
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-07-01 11:37:45 +0200

    getMessagesWith (connected user)

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -24,6 +25,26 @@ class ChatController extends Controller
 
         return view("chat", [
             "users" => $users
+        ]);
+    }
+
+    public function getMessagesWith(Request $request, $connectedUserId): View
+    {
+        $currentUserId = $request->user()->id;
+
+        $messages = Message::where(function ($query) use ($connectedUserId) {
+            $query
+            ->where("sender", $connectedUserId)
+            ->orWhere("receiver", $connectedUserId);
+        })
+        ->where(function ($query) use ($currentUserId) {
+            $query
+            ->where("sender", $currentUserId)
+            ->orWhere("receiver", $currentUserId);
+        })->orderBy("created_at")->get();
+
+        return view("chat")->with([
+            "messages" => $messages
         ]);
     }
 }

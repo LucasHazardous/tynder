819a1be8afc7ee129b8ade29071079d5aac3ffb6
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-07-01 11:36:38 +0200

    create messages table

---
+++ database/migrations/2023_07_01_085831_create_messages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('sender');
+            $table->unsignedBigInteger('receiver');
+            $table->foreign("sender")->references("id")->on("users");
+            $table->foreign("receiver")->references("id")->on("users");
+            $table->string("content");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};

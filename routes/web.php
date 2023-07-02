2cd4489bb7a793feb4fc7c82278659c73ff3b485
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-07-02 22:07:02 +0200

    logo redirects to welcome page

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name("welcome");
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

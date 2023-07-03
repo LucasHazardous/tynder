1f4cc684b6b93f3b9d80d3a349b5bb309b068e72
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-07-03 17:11:31 +0200

    only download avatar on downloadAvatar usage

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,21 +19,10 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        $imgData = json_decode(file_get_contents("https://nekos.best/api/v2/waifu"));
-        $url = $imgData->results[0]->url;
-
-        $info = pathinfo($url);
-        $contents = file_get_contents($url);
-        $file = '/tmp/' . $info['basename'];
-        file_put_contents($file, $contents);
-        $uploaded_file = new UploadedFile($file, $info['basename']);
-        
-        $avatar = Storage::disk("public")->put('avatar', $uploaded_file);
-        
         return [
             'name' => fake()->firstName('female'),
-            'avatar' => $avatar,
-            'description' => $imgData->results[0]->source_url,
+            'avatar' => null,
+            'description' => null,
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
@@ -49,5 +38,26 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public function downloadAvatar(): static
+    {
+        return $this->state(function (array $attributes) {
+            $imgData = json_decode(file_get_contents("https://nekos.best/api/v2/waifu"));
+            $url = $imgData->results[0]->url;
+    
+            $info = pathinfo($url);
+            $contents = file_get_contents($url);
+            $file = '/tmp/' . $info['basename'];
+            file_put_contents($file, $contents);
+            $uploaded_file = new UploadedFile($file, $info['basename']);
+            
+            $avatar = Storage::disk("public")->put('avatar', $uploaded_file);
+
+            return [
+                'avatar' => $avatar,
+                'description' => $imgData->results[0]->source_url,
+            ];
+        });
     }
 }

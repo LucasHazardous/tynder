c5e52989a1eaa5ca7413f2bf7ceec046ff850ed3
LucasHazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-08-11 19:55:51 +0200

    NewMessage event

---
+++ app/Events/NewMessage.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NewMessage implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+
+    public $message;
+    public $receiver;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($message, $receiver)
+    {
+        $this->message = $message;
+        $this->receiver = $receiver;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel("messages.". $this->receiver)
+        ];
+    }
+}
